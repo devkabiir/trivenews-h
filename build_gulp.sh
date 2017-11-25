915da6c6ef8b27441fb432ba131a66b04b4f4d19
Lex Berezhny <lex@damoti.com>
2017-11-24 22:44:23 -0500

    redirect all npm and gulp output to dev/null

--- build_gulp.sh
+++ build_gulp.sh
@@ -1,3 +1,3 @@
-npm install --production
-NODE_ENV=production node_modules/.bin/gulp build > /dev/null
-npm cache clean
+npm install --production &>/dev/null
+NODE_ENV=production node_modules/.bin/gulp build &>/dev/null
+npm cache clean &>/dev/null
